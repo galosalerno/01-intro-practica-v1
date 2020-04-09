d644910954f7457fbb358e05d0da26b5d7e8b2cd
galosalerno <galosalerno@gmail.com>
2020-04-09 18:39:45 -0300

    ejemplo

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -1,8 +1,21 @@
 package ar.edu.unahur.obj2.ejercicio1;
-
+/*
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public int ordenar() {
+
+        int x = 10 ;
+                return x;
     }
+}
+
+ */
+public class Ejercicio1 {
+
+       public static void main(String args[]){
+
+           int x=10;
+           System.out.printIn("hola mundo " +x+ "chau");
+
+       }
 }
